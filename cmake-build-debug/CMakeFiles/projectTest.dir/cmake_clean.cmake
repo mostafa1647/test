9aa6aa96edd5fbb74a71d6286aff0303a96f771d
Mostafa <mojtahedimostafa.m@gmail.com>
2018-12-28 21:34:16 +0330

    first

---
+++ cmake-build-debug/CMakeFiles/projectTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projectTest.dir/main.cpp.o"
+  "projectTest.pdb"
+  "projectTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projectTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
